67873742a72b84afe90f4170da3d57739f4e8dd5
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-08-19 19:05:17 +0330

    implemented object literals

--- index-parsa.js
+++ index-parsa.js
@@ -1,1 +1,15 @@
-let circle = {}
+// object literal
+let circle = {
+  // properties
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  // methods
+  draw: function () {
+    console.log("draw circle");
+  },
+};
+
+circle.draw()
